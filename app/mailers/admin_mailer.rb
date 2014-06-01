a41e5ca41d7accef867596ec82bbeddd79edcc93
Owen Kelly <owen.james.kelly@gmail.com>
2014-06-01 15:32:18 -0700

    mailgunner fun, now with admin addresses

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,5 +1,5 @@
 class AdminMailer < ActionMailer::Base
-  default from: "admin@boiling-ridge-6865.herokuapp.com"
+  default from: "owen.james.kelly@gmail.com"
 
   	def welcome_new user
   		@url = new_user_session_path
